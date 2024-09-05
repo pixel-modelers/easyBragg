1d8d7fcb9d9383e016bda10dbd7170908dfe45bc
dermen <dermen@lbl.gov>
2024-09-04 19:36:05 -0700

    update submods and example

--- example.py
+++ example.py
@@ -1,9 +1,11 @@
 from simtbx.nanoBragg import nanoBragg
+from simtbx.nanoBragg import shapetype
 import pylab as plt
 
 N = nanoBragg()
 N.default_F=1
 N.Ncells_abc=10,10,10
+N.xtal_shape = shapetype.Gauss
 N.add_nanoBragg_spots()
 img = N.raw_pixels.as_numpy_array()
 N.adc_offset_adu = img.max()*0.1
